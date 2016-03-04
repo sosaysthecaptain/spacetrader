cd2922f533bb00368336a7769affd44bfe6a58b1
Marc Auger <sosaysthecaptain@gmail.com>
2016-03-03 21:10:51 -0600

    steppers in BYOS work

--- SpaceTrader/Design2VC.swift
+++ SpaceTrader/Design2VC.swift
@@ -37,16 +37,8 @@ class Design2VC: UIViewController {
     @IBOutlet weak var cancelDesignOutlet: CustomButton!
     @IBOutlet weak var constructShipOutlet: CustomButton!
     
-    var cargoBays = 0
-    var range = 0
-    var hullStrength = 0
-    var weaponSlots = 0
-    var shieldSlots = 0
-    var gadgetSlots = 0
-    var crewQuarters = 0
+    var size: SizeType = SizeType.Medium        // set from player on viewDidLoad
     
-    // next, max and min variables
-    // then function to display relevant variables to screen
     // then function to set initial values based on chosen ship size
     // then plug steppers into variables, within max/min limits
     
@@ -56,7 +48,104 @@ class Design2VC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        size = player.selectedConstructShipSize
+        self.setMaxMinDefault()
+        self.updateDisplay()
+    }
+    
+    func updateDisplay() {
+        cargoBaysField.text = "\(Int(cargoBaysStepper.value))"
+        rangeField.text = "\(Int(rangeStepper.value))"
+        hullStrengthField.text = "\(Int(hullStrengthStepper.value))"
+        weaponSlotsField.text = "\(Int(weaponSlotsStepper.value))"
+        shieldSlotsField.text = "\(Int(shieldSlotsStepper.value))"
+        gadgetSlotsField.text = "\(Int(gadgetSlotsStepper.value))"
+        crewQuartersField.text = "\(Int(crewQuartersStepper.value))"
+    }
+    
+    func setMaxMinDefault() {
+        // set default mins and maxes
+        cargoBaysStepper.minimumValue = 5
+        cargoBaysStepper.maximumValue = 200
+        
+        rangeStepper.minimumValue = 5
+        rangeStepper.maximumValue = 20
+        
+        hullStrengthStepper.minimumValue = 10
+        hullStrengthStepper.maximumValue = 1000
+        hullStrengthStepper.stepValue = 10
+        
+        weaponSlotsStepper.minimumValue = 0
+        weaponSlotsStepper.maximumValue = 5
+        
+        shieldSlotsStepper.minimumValue = 0
+        shieldSlotsStepper.maximumValue = 5
+        
+        gadgetSlotsStepper.minimumValue = 0
+        gadgetSlotsStepper.maximumValue = 5
+        
+        crewQuartersStepper.minimumValue = 1
+        crewQuartersStepper.maximumValue = 5
+        
+        // set mins that vary and all defaults
+        switch size {
+            case SizeType.Tiny:
+                rangeStepper.minimumValue = 5
+                hullStrengthStepper.minimumValue = 10
+                
+                cargoBaysStepper.value = 5
+                rangeStepper.value = 12
+                hullStrengthStepper.value = 50
+                weaponSlotsStepper.value = 0
+                shieldSlotsStepper.value = 0
+                gadgetSlotsStepper.value = 0
+                crewQuartersStepper.value = 1
+            case SizeType.Small:
+                rangeStepper.minimumValue = 5
+                hullStrengthStepper.minimumValue = 25
+            
+                cargoBaysStepper.value = 15
+                rangeStepper.value = 14
+                hullStrengthStepper.value = 100
+                weaponSlotsStepper.value = 1
+                shieldSlotsStepper.value = 1
+                gadgetSlotsStepper.value = 1
+                crewQuartersStepper.value = 1
+            case SizeType.Medium:
+                rangeStepper.minimumValue = 5
+                hullStrengthStepper.minimumValue = 50
+            
+                cargoBaysStepper.value = 25
+                rangeStepper.value = 14
+                hullStrengthStepper.value = 150
+                weaponSlotsStepper.value = 2
+                shieldSlotsStepper.value = 1
+                gadgetSlotsStepper.value = 1
+                crewQuartersStepper.value = 2
+            case SizeType.Large:
+                rangeStepper.minimumValue = 15
+                hullStrengthStepper.minimumValue = 100
+            
+                cargoBaysStepper.value = 35
+                rangeStepper.value = 14
+                hullStrengthStepper.value = 200
+                weaponSlotsStepper.value = 2
+                shieldSlotsStepper.value = 2
+                gadgetSlotsStepper.value = 2
+                crewQuartersStepper.value = 3
+            case SizeType.Huge:
+                rangeStepper.minimumValue = 14
+                hullStrengthStepper.minimumValue = 150
+            
+                cargoBaysStepper.value = 50
+                rangeStepper.value = 14
+                hullStrengthStepper.value = 250
+                weaponSlotsStepper.value = 3
+                shieldSlotsStepper.value = 2
+                gadgetSlotsStepper.value = 2
+                crewQuartersStepper.value = 3
+        }
+        
     }
     
     @IBAction func cancelDesignAction(sender: AnyObject) {
@@ -65,6 +154,33 @@ class Design2VC: UIViewController {
     @IBAction func constructShipAction(sender: AnyObject) {
     }
     
+    // stepper set functions--not at all interesting
+    @IBAction func cargoBaysStepperSet(sender: AnyObject) {
+        updateDisplay()
+    }
     
+    @IBAction func rangeBaysStepperSet(sender: AnyObject) {
+        updateDisplay()
+    }
+    
+    @IBAction func hullStrengthStepperSet(sender: AnyObject) {
+        updateDisplay()
+    }
+    
+    @IBAction func weaponSlotStepperSet(sender: AnyObject) {
+        updateDisplay()
+    }
+    
+    @IBAction func shieldSlotsStepperSet(sender: AnyObject) {
+        updateDisplay()
+    }
+    
+    @IBAction func gadgetsSlotStepperSet(sender: AnyObject) {
+        updateDisplay()
+    }
+    
+    @IBAction func crewQuartersStepperSet(sender: AnyObject) {
+        updateDisplay()
+    }
     
 }
