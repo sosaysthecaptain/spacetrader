71a272ae5b24b270ff941b9dafc60950bd125a44
Marc Auger <sosaysthecaptain@gmail.com>
2016-03-15 10:43:54 -0500

    tab bar icons set

--- SpaceTrader/AppDelegate.swift
+++ SpaceTrader/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // set tab bar tint color
+        UITabBar.appearance().tintColor = mainPurple
+        
         return true
     }
 
