389dab009b5b2aa48fd2baf58821686d14b5413c
Marc Auger <sosaysthecaptain@gmail.com>
2016-03-18 22:01:02 -0500

    buy picker fully implemented

--- SpaceTrader/BuyPickerVCOLD.swift
+++ SpaceTrader/BuyPickerVCOLD.swift
@@ -9,14 +9,13 @@
 import UIKit
 
 // this is the modern version of BuyModalVC
-class BuyPickerVC: UIViewController {
-    
+class BuyPickerVCOLD: UIViewController {
+
     @IBOutlet weak var firstLabel: StandardLabel!
     @IBOutlet weak var secondLabel: StandardLabel!
     @IBOutlet weak var quantityLabel: PurpleHeader!
     
     @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var buyButton: PurpleButtonTurnsGray!
     
     let commodity = buySellCommodity!                        // this is stored as a global
     let max = player.getMax(buySellCommodity!)
@@ -24,11 +23,8 @@ class BuyPickerVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // set color of slider
-        slider.tintColor = mainPurple
-        
         // set page title
-        self.title = "Buy \(commodity.rawValue)"
+        // TODO
         
         // set slider max and min
         slider.minimumValue = 0
@@ -40,16 +36,9 @@ class BuyPickerVC: UIViewController {
     
     func updateUI() {
         // set labels
-        firstLabel.text = "You can buy up to \(max) bays of \(commodity.rawValue)."
+        //firstLabel.text = "You can buy up to \(max) bays of \(commodity.rawValue)."
         secondLabel.text = "How many would you like to buy?"
         quantityLabel.text = "\(Int(slider.value)) bays"
-        
-        // disable buy if quantity is zero
-        if slider.value == 0 {
-            buyButton.enabled = false
-        } else {
-            buyButton.enabled = true
-        }
     }
     
     @IBAction func sliderMoved(sender: AnyObject) {
@@ -57,12 +46,7 @@ class BuyPickerVC: UIViewController {
     }
     
     @IBAction func buyPressed(sender: AnyObject) {
-        // submit transaction
-        player.buy(commodity, quantity: Int(slider.value))
-        
-        // close VC
-        navigationController?.popToRootViewControllerAnimated(true)
     }
     
-    
+
 }
