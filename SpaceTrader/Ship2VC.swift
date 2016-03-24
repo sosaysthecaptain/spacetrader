e43f6cc4f36d97417930834e5192d0f70b881a9e
Marc Auger <sosaysthecaptain@gmail.com>
2016-03-24 14:47:52 -0500

    about to rebuild ship page

---
+++ SpaceTrader/Ship2VC.swift
@@ -0,0 +1,20 @@
+//
+//  Ship2VC.swift
+//  SpaceTrader
+//
+//  Created by Marc Auger on 3/24/16.
+//  Copyright © 2016 Marc Auger. All rights reserved.
+//
+
+import UIKit
+
+class Ship2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
